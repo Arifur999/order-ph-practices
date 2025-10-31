fb2fd0dddfe8db7a8f28d736de9e1828edd59643
Arifur999 <arr96777@gmail.com>
2025-11-01 01:11:50 +0600

    added set and order of n

---
+++ databaseSet.js
@@ -0,0 +1,67 @@
+const set=new set()
+
+set.add("arif");
+set.add("sakib");
+
+
+console.log(set);
+
+// ---------------------------------------------------
+
+
+const arr = ["mengo" , "banana", ]
+const set =new set(arr);
+const test =Array.form(arr);
+console.log(test);
+
+
+
+
+// arry diye new ekta object return kore 
+
+
+
+const chack= set.has("mango")
+
+
+// set.has diye kno kisu ase naki oita chack kora hoy
+
+
+const delete =set.delete("arif")
+
+// kno kisu delete korar jonno set.delete use hoy bulian valu dey and delete kore day
+
+
+// --------------------------------------------------
+
+
+const arr =["shadw","safi","hamza"]
+
+const removeItem =(arr)=>{
+const newArr=[]
+arr.forEach(element => {
+    
+    if (!newArr.includes(element)) {
+        newArr.push(element);
+    }
+});
+
+    return arr;
+}
+
+
+// set diye
+
+
+const arr =["shadw","safi","hamza"]
+
+const removeItem =(arr)=>{
+ const set= new set (arr)
+return Array.form(set)
+
+}
+
+console.log(removeItem(arr));
+
+
+// ------------------------------------------------------------
